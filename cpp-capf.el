9cbd59e88706696d6a271c08a65fb19384da119c
Philip K <philip@warpmail.net>
2019-06-09 08:05:25 +0200

    converted all tabs to spaces

--- cpp-capf.el
+++ cpp-capf.el
@@ -16,9 +16,9 @@
 
 (defcustom cpp-capf-include-paths
   '("/usr/local/include"
-	"/usr/lib/llvm-7/lib/clang/7.0.1/include"
-	"/usr/include/x86_64-linux-gnu"
-	"/usr/include" "." ".." "../..")
+    "/usr/lib/llvm-7/lib/clang/7.0.1/include"
+    "/usr/include/x86_64-linux-gnu"
+    "/usr/include" "." ".." "../..")
   "Paths to directories with header files."
   :type 'list
   :group 'cpp-capf)
@@ -37,45 +37,45 @@
   "Function used for ‘completion-at-point-functions’ in by
 `cpp-completion-at-point-function'."
   (let* ((temp (generate-new-buffer " *clang*")))
-	(prog2
-		(apply
-		 #'call-process-region
-		 (append (list (point-min) (point-max)
-					   cpp-capf-clang nil temp nil
-					   "-cc1" "-fsyntax-only"
-					   "-code-completion-macros")
-				 (mapcar (apply-partially #'concat "-I")
-						 cpp-capf-include-paths)
-				 cpp-capf-extra-flags
-				 (list (format
-						"-code-completion-at=-:%d:%d"  
-						(line-number-at-pos)
-						(1+ (length (encode-coding-region
-									 (line-beginning-position)
-									 (point) 'utf-8 t))))
-					   "-")))
-	  (with-current-buffer temp
-		(goto-char (point-min))
-		(let (result)
-		  (while (search-forward-regexp
-				  "^COMPLETION: \\(.+\\) : "
-				  nil t)
-			(push (match-string 1) result))
-		  result))
-	  (kill-buffer temp))))
+    (prog2
+        (apply
+         #'call-process-region
+         (append (list (point-min) (point-max)
+                       cpp-capf-clang nil temp nil
+                       "-cc1" "-fsyntax-only"
+                       "-code-completion-macros")
+                 (mapcar (apply-partially #'concat "-I")
+                         cpp-capf-include-paths)
+                 cpp-capf-extra-flags
+                 (list (format
+                        "-code-completion-at=-:%d:%d"
+                        (line-number-at-pos)
+                        (1+ (length (encode-coding-region
+                                     (line-beginning-position)
+                                     (point) 'utf-8 t))))
+                       "-")))
+      (with-current-buffer temp
+        (goto-char (point-min))
+        (let (result)
+          (while (search-forward-regexp
+                  "^COMPLETION: \\(.+\\) : "
+                  nil t)
+            (push (match-string 1) result))
+          result))
+      (kill-buffer temp))))
 
 ;;;###autoload
 (defun cpp-completion-at-point-function ()
   "Return list of items for `completion-at-point' for completing
 C code."
   (unless cpp-capf-clang
-	(error "company either not installed or not in path"))
+    (error "company either not installed or not in path"))
   (list (save-excursion
-		  (unless (memq (char-before) '(?\. ?\t ?\n ?\ ?\; ?\)))
-			(forward-word -1))
-		  (point))
-		(point)
-		(completion-table-with-cache #'cpp-capf--completions)
-		:exclusive 'no))
+          (unless (memq (char-before) '(?\. ?\t ?\n ?\ ?\; ?\)))
+            (forward-word -1))
+          (point))
+        (point)
+        (completion-table-with-cache #'cpp-capf--completions)
+        :exclusive 'no))
 
 (provide 'cpp-capf)
